315af5e1f89148d8617e8ef30cf80b6a3b70a669
linki-yoshi <oojioota@hotmail.com>
2018-10-01 10:48:01 +0800

    update

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ app.use(express.static('public'));
 const appRoute = require('./src/routes/route.js'); 
 appRoute(app);
 
-const port = 3000;
+// const port = 3000;
 
-app.listen(port, function () {
-   console.log(`Server is up and running on port numner ${port}`);
-});
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+    app.listen(port, ip);
+
+// app.listen(port, function () {
+//    console.log(`Server is up and running on port numner ${port}`);
+// });
