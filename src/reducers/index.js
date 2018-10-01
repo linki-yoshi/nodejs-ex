e2f04df8a7b0b5a0f7cbfc33c7d2a0200317aefc
hfgl-yilin <biel.cslayman@gmail.com>
2018-10-01 10:01:16 +0800

    update

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import productsReducer from "./productsReducer";
-import cartReducer from "./cartReducer";
-
-export default combineReducers({
-    products: productsReducer,
-    cart: cartReducer
-});
